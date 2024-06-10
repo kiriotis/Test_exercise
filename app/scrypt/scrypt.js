471b579ef83661cb7e4bdb2b5a6c7c8a1709d222
ivan <ivan@DESKTOP-NELLSAH>
2024-06-10 21:30:46 +0300

    Feat:add allerts to form

--- app/scrypt/scrypt.js
+++ app/scrypt/scrypt.js
@@ -11,3 +11,30 @@ let buttonClose = document
   .addEventListener("click", () => {
     document.querySelector(".popUP_container").classList.add("popUP_Disable");
   });
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let data = [];
+  document.querySelectorAll("input").forEach((input) => {
+    if (input.type == "checkbox") {
+      data.push(input.checked.toString());
+    } else {
+      data.push(input.value.toString());
+    }
+  });
+
+  if (data[0] == "") {
+    alert("Введите, введите свой логин");
+  } else {
+    alert("Ваш логин: " + data[0]);
+  }
+
+  if (data[1] == "") {
+    alert("Введите, введите свой пароль");
+  } else {
+    alert("Ваш пароль: " + data[1]);
+  }
+  if (data[2]) {
+    alert("Ваш пароль: " + data[2]);
+  }
+});
